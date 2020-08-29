67da6c9d640cf80222385063ab0d40527d56577a
doob187 <amderkum@gmail.com>
2020-08-29 15:21:09 +0200

    push

--- root/app/startupp-mounts.sh
+++ root/app/startupp-mounts.sh
@@ -24,3 +24,19 @@ for i in ${mounts[@]}; do
     log $i ${SMOUNT}/$i.mounted
 done
 
+sleep 10
+
+/usr/bin/mergerfs -o sync_read,auto_cache,dropcacheonclose=true,use_ino,allow_other,func.getattr=newest,category.create=ff,minfreespace=0,fsname=mergerfs /mnt/drive-*\ /mnt/unionfs
+
+MERGERFS_PID=$(pgrep mergerfs)
+log "PID: ${MERGERFS_PID}"
+
+while true; do
+
+  if [ -z "${MERGERFS_PID}" ] || [ ! -e /proc/${MERGERFS_PID} ]; then
+     fusermount -uz  /mnt/unionfs
+    /usr/bin/mergerfs -o sync_read,auto_cache,dropcacheonclose=true,use_ino,allow_other,func.getattr=newest,category.create=ff,minfreespace=0,fsname=mergerfs /mnt/drive-*\ /mnt/unionfs
+    MERGERFS_PID=$(pgrep mergerfs)
+  fi
+  sleep 10s
+done
