628739058e2596bb0905169be4abbf1da056133d
doob187 <60312740+doob187@users.noreply.github.com>
2020-09-12 00:17:07 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="25" charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/tynor88/docker-templates/master/images/rclone_small.png">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
