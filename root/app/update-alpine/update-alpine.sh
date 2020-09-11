b9ae937c09fdad14d25669f2bb47d687be63fd6e
doob187 <60312740+doob187@users.noreply.github.com>
2020-09-12 01:03:29 +0200

    Update update-alpine.sh

--- root/app/update-alpine/update-alpine.sh
+++ root/app/update-alpine/update-alpine.sh
@@ -4,18 +4,6 @@ function log() {
 echo "[Mount] ${1}"
 }
 ## function source start
-IFS=$'\n'
-filter="$1"
-config=/config/rclone/rclone-docker.conf
-#rclone listremotes | gawk "$filter"
-mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter" | sed -e 's/tcrypt://g' | sed -e 's/gcrypt://g' | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d')
-## function source end
-for i in ${mounts[@]}; do
-  rclone dedupe skip $i: --config=${config} --drive-use-trash=false --no-traverse --transfers=50 --user-agent="SomeLegitUserAgent" 
-  rclone rmdirs $i: --config=${config} --drive-use-trash=false --fast-list --transfers=50 --user-agent="SomeLegitUserAgent" 
-  rclone delete $i: --config=${config} --fast-list --drive-trashed-only --drive-use-trash=false --transfers 50 --user-agent="SomeLegitUserAgent" 
-  rclone cleanup $i: --config=${config} --user-agent="SomeLegitUserAgent" 
-done
 log "-> update rclone || start <-"
     apk add unzip --quiet
     curl --no-progress-meter https://rclone.org/install.sh | bash -s beta >/dev/null 2>&1
