1921a64e728c931cbf6e81b8e538b67b94a5ef5b
doob187 <amderkum@gmail.com>
2020-10-14 22:40:44 +0200

    stop mountsize

--- root/app/mountsize/mountsize.sh
+++ root/app/mountsize/mountsize.sh
@@ -3,21 +3,21 @@
 # Copyright (c) 2020, MrDoob
 # All rights reserved.
 # 
-## function source start
-while true; do
-IFS=$'\n'
-filter=$1
-## function source end
-config=/config/rclone/rclone-docker.conf
-mapfile -t mountscrypt < <(eval rclone listremotes --config=${config} | grep "$filter" | grep "$filter" | sed -e 's/tdrive//g' | sed -e 's/gdrive//g' | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d' | sort -r)
-## function source end
-for i in ${mountscrypt[@]}; do
-  echo -e "$(rclone size $i: --config=${config} --fast-list --user-agent="SomeLegitUserAgent")"  > /config/logs/mountsize-$i.log
-done
-mapfile -t mountsuncrypt < <(eval rclone listremotes --config=${config} | grep "$filter" | grep "$filter" | sed -e 's/tcrypt//g' | sed -e 's/gcrypt//g' | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d' | sort -r)
-for i in ${mountsuncrypt[@]}; do
-  echo -e "$(rclone size $i: --config=${config} --fast-list --exclude="**encrypt**" --user-agent="SomeLegitUserAgent")"  > /config/logs/mountsize-$i.log
-done
-##sleep for next day##
-sleep $(($(date -f - +%s- <<< $'tomorrow 00:30\nnow')0))
-done
+# ## function source start
+# while true; do
+# IFS=$'\n'
+# filter=$1
+# ## function source end
+# config=/config/rclone/rclone-docker.conf
+# mapfile -t mountscrypt < <(eval rclone listremotes --config=${config} | grep "$filter" | grep "$filter" | sed -e 's/tdrive//g' | sed -e 's/gdrive//g' | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d' | sort -r)
+# ## function source end
+# for i in ${mountscrypt[@]}; do
+  # echo -e "$(rclone size $i: --config=${config} --fast-list --user-agent="SomeLegitUserAgent")"  > /config/logs/mountsize-$i.log
+# done
+# mapfile -t mountsuncrypt < <(eval rclone listremotes --config=${config} | grep "$filter" | grep "$filter" | sed -e 's/tcrypt//g' | sed -e 's/gcrypt//g' | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d' | sort -r)
+# for i in ${mountsuncrypt[@]}; do
+  # echo -e "$(rclone size $i: --config=${config} --fast-list --exclude="**encrypt**" --user-agent="SomeLegitUserAgent")"  > /config/logs/mountsize-$i.log
+# done
+# ##sleep for next day##
+# sleep $(($(date -f - +%s- <<< $'tomorrow 00:30\nnow')0))
+# done
