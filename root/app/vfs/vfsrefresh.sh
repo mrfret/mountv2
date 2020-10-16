10cbbe4a50bc23f82be925b8276a408144691b13
doob187 <60312740+doob187@users.noreply.github.com>
2020-10-17 00:18:20 +0200

    Update vfsrefresh.sh

--- root/app/vfs/vfsrefresh.sh
+++ root/app/vfs/vfsrefresh.sh
@@ -5,28 +5,58 @@
 # 
 # ## function source start
 
-# wait for full started mergerfs 
-## pgrep mergerfs && read each pgrep drive-$i 
-## wait command 30 secs after startup
+VFS_REFRESH=${VFS_REFRESH}
 
-# list time command for refresh
-## command here 
-## rclone.env ! VALUE !
-## basic setting  sed -i h from DIR_CACHE_TIME 
-## else based
+function refresh() {
+SRC=/config/rc-refresh
+SCACHE=/tmp/rclone_cache
+IFS=$'\n'
+filter="$1"
+config=/config/rclone/rclone-docker.conf
+mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter" | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d')
+##### RUN MOUNT #####
+for i in ${mounts[@]}; do
+  bash ${SRC}/$i-rc-file.sh
+  chmod a+x ${SRC}/$i-rc-file.sh
+  chown -hR abc:abc ${SRC}/$i-rc-file.sh
+  sleep 5
+done
+}
 
-# list remotes
-## command here 
+function checkmergerfs() {
+MERGERFS_PID=$(pgrep mergerfs)
+if [ -z "${MERGERFS_PID}" ]; then
+    sleep 5
+    checkmergerfs
+fi
+}
 
-# list files for vfs/refresh files
-## command here
-## per find command -exec *?*
-## eval command *?*
-## execute vfs/refresh for restart/startup
-## command here
+function drivecheck() {
 
-# UI addon *?*
+checkmergerfs
 
-# while true based *?*
+IFS=$'\n'
+filter="$1"
+config=/config/rclone/rclone-docker.conf
+#rclone listremotes | gawk "$filter"
+mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter" | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d')
+for i in ${mounts[@]}; do
+  run=$(ls /mnt/drive-$i/ | wc -l)
+  pids="$(ps -ef | grep '$i-mount.sh' | head -n 1 | grep -v grep | awk '{print $1}' | wc -l)"
+  if [[ "$run" != "0" && "$pids" != "0" ]]; then
+     sleep 30
+  else
+     refresh
+  fi
+done
+}
 
-# loop mode *?*
+while true; do
+
+if [[ "$( ${VFS_REFRESH} | sed -e 's/h//g')" != 'null' ]]; then
+   drivecheck
+   sleep ${VFS_REFRESH}
+fi
+done
+#UI addon *?*
+#<EOF>#
