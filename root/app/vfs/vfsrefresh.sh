f38290e95efa1b4d75dfa9e5b94f324f46126971
doob187 <60312740+doob187@users.noreply.github.com>
2020-10-16 22:28:26 +0200

    Create vfsrefresh.sh

---
+++ root/app/vfs/vfsrefresh.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+# Copyright (c) 2020, MrDoob
+# All rights reserved.
+# 
+# ## function source start
+
+# wait for full started mergerfs 
+## pgrep mergerfs && read each pgrep drive-$i 
+## wait command 30 secs after startup
+
+# list time command for refresh
+## command here 
+## rclone.env ! VALUE !
+## basic setting  sed -i h from DIR_CACHE_TIME 
+## else based
+
+# list remotes
+## command here 
+
+# list files for vfs/refresh files
+## command here
+## per find command -exec *?*
+## eval command *?*
+## execute vfs/refresh for restart/startup
+## command here
+
+# UI addon *?*
+
+# while true based *?*
+
+# loop mode *?*
