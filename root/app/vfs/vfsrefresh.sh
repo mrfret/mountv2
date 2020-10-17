aee9fc2bccbdceaa1ee3c990da66fa9b9beee44b
doob187 <60312740+doob187@users.noreply.github.com>
2020-10-17 19:20:06 +0200

    Update vfsrefresh.sh

--- root/app/vfs/vfsrefresh.sh
+++ root/app/vfs/vfsrefresh.sh
@@ -51,9 +51,9 @@ config=/config/rclone/rclone-docker.conf
 #rclone listremotes | gawk "$filter"
 mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter" | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d')
 for i in ${mounts[@]}; do
-  run=$(ls /mnt/drive-$i/ | wc -l)
-  pids="$(ps -ef | grep '$i' | head -n 1 | grep -v grep | awk '{print $1}' | wc -l)"
-  if [[ "$run" != '0' && "$pids" != '0' ]]; then
+  run=$(ls -la /mnt/drive-$i/ | wc -l)
+  pids=$(ps -ef | grep '$i' | head -n 1 | awk '{print $1}')
+  if [ "$pids" != '0' ] && [ "$run" != '0' ]; then
      refresh
   else
      sleep 30
