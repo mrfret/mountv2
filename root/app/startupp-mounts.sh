3ce2825bd4a284da018c42f217b5341124404208
doob187 <60312740+doob187@users.noreply.github.com>
2021-05-07 00:14:24 +0200

    fusermount

--- root/app/startupp-mounts.sh
+++ root/app/startupp-mounts.sh
@@ -57,10 +57,10 @@ SRC=/config/rc-refresh
 IFS=$'\n'
 filter="$1"
 config=/config/rclone/rclone-docker.conf
-POINTS="/mnt/unionfs /mnt/remotes"
+POINTS="unionfs remotes"
 for mount in ${POINTS};do
-    command=$(mountpoint -q $mount && echo true || echo false)
-    if [[ $command == "false" ]];then fusermount -uzq $mount;fi
+    command=$(mountpoint -q /mnt/$mount && echo true || echo false)
+    if [[ $command == "false" ]];then fusermount -uzq /mnt/$mount 1>/dev/null 2>&1;fi
 done
 if [[ ! -d "/mnt/unionfs" ]];then mkdir -p /mnt/unionfs;fi
 if [[ -f "/config/scripts/union-mount.sh" ]];then
